7e9bc81ffd8734de5ee194cf5c365516c534de38
alfonsogamer360 <gutierrezcabrerajeremias@gmail.com>
2026-03-06 22:45:23 -0300

    Versão melhorada da anterior

--- sss.js
+++ sss.js
@@ -35,12 +35,14 @@ function vendasMesCategoria(categoria) {
 }
 // Caso não seja no faturamento:
 function listaComissão() {
-    for (let a = 0; a < categorias.length; a++) {
-        let categoriaAgora = categorias[a];
-        for (let i = 0; i < categoriaAgora.length; i++) {
-        if (categoriaAgora[i].Preço > 2000) {
-            console.log(`Produto: ${categoriaAgora[i].nomeProduto} \nComissão extra total: R$${((categoriaAgora[i].Preço * 0.03) * categoriaAgora[i].Vendas).toFixed(2)}`)
-        }
+    console.log("\n-----Comissões-----");
+    for (let a = 0; a < categorias.length; a++) { //Percorre por todo o array categorias
+        let categoriaAgora = categorias[a]; // a categoria atual vai ser armazenada nesta variavel
+        for (let i = 0; i < categoriaAgora.length; i++) { //Percorre o array da categoria atual 
+            let produto = categoriaAgora[i] // O nome, vendas, e preço do produto atual da categoria atual será armazenada nesta variavel
+            if (produto.Preço > 2000) { // se verdadeiro, envia na console a comissão extra total
+                console.log(`Produto: ${produto.nomeProduto} \nComissão extra total: R$${((produto.Preço * 0.03) * produto.Vendas).toFixed(2)}`)
+            }
         }
     }
 }
