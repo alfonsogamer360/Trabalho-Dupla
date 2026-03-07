978c9737d5119febb96cf191f8d77268437a35b7
alfonsogamer360 <gutierrezcabrerajeremias@gmail.com>
2026-03-06 23:31:13 -0300

    função produtoMaisCaroVendido

--- sss.js
+++ sss.js
@@ -1,17 +1,17 @@
 let eletrodomesticos = [
-    {nomeProduto: "Tv LG 32", Vendas: 13, Preço: 850.41},
-    {nomeProduto: "Geladeira", Vendas: 12, Preço: 454.21},
-    {nomeProduto: "Micro-ondas", Vendas: 19, Preço: 519.99},
+    {nomeProduto: "Tv LG 32", Vendas: 13, Preco: 850.41},
+    {nomeProduto: "Geladeira", Vendas: 12, Preco: 454.21},
+    {nomeProduto: "Micro-ondas", Vendas: 19, Preco: 519.99},
 ];
 let moveis = [
-    {nomeProduto: "Estante Modular 4 Prateleiras", Vendas: 22, Preço: 52.50},
-    {nomeProduto: "Cadeira de Plástico Dobrável", Vendas: 41, Preço: 55.75},
-    {nomeProduto: "Cama Box Casal", Vendas: 10, Preço: 648},
+    {nomeProduto: "Estante Modular 4 Prateleiras", Vendas: 22, Preco: 52.50},
+    {nomeProduto: "Cadeira de Plástico Dobrável", Vendas: 41, Preco: 55.75},
+    {nomeProduto: "Cama Box Casal", Vendas: 10, Preco: 648},
 ];
 let eletronicos = [
-    {nomeProduto: "iPhone 11", Vendas: 5, Preço: 1499.00},
-    {nomeProduto: "Redmi Note 14", Vendas: 8, Preço: 1050.00},
-    {nomeProduto: "Samsung S24", Vendas: 6, Preço: 2599.51},
+    {nomeProduto: "iPhone 11", Vendas: 5, Preco: 1499.00},
+    {nomeProduto: "Redmi Note 14", Vendas: 8, Preco: 1050.00},
+    {nomeProduto: "Samsung S24", Vendas: 6, Preco: 2599.51},
 ];
 let categorias = [eletrodomesticos,moveis,eletronicos];
 
@@ -20,7 +20,7 @@ function vendasMesTotal(){
     for (let a = 0; a < categorias.length; a++){
         let categoriaAgora = categorias[a]
         for (let i = 0; i < categoriaAgora.length; i++) {
-        totalCategoria += (categoriaAgora[i].Preço * categoriaAgora[i].Vendas)
+        totalCategoria += (categoriaAgora[i].Preco * categoriaAgora[i].Vendas)
     }
 }
     return `O faturamento total da categoria é: R$${totalCategoria.toFixed(2)}`
@@ -29,23 +29,38 @@ function vendasMesTotal(){
 function vendasMesCategoria(categoria) {
     let totalCategoria = 0
     for (let i = 0; i < categoria.length; i++) {
-        totalCategoria += (categoria[i].Preço * categoria[i].Vendas)
+        totalCategoria += (categoria[i].Preco * categoria[i].Vendas)
     }
     return `O faturamento da categoria é: R$${totalCategoria.toFixed(2)}`
 }
 // Caso não seja no faturamento:
 function listaComissão() {
     console.log("\n-----Comissões-----");
-    for (let a = 0; a < categorias.length; a++) { //Percorre por todo o array categorias
+    for (let a = 0; a < categorias.length; a++) { //Percorre por todo o categoriaAgora categorias
         let categoriaAgora = categorias[a]; // a categoria atual vai ser armazenada nesta variavel
-        for (let i = 0; i < categoriaAgora.length; i++) { //Percorre o array da categoria atual 
-            let produto = categoriaAgora[i] // O nome, vendas, e preço do produto atual da categoria atual será armazenada nesta variavel
-            if (produto.Preço > 2000) { // se verdadeiro, envia na console a comissão extra total
-                console.log(`Produto: ${produto.nomeProduto} \nComissão extra total: R$${((produto.Preço * 0.03) * produto.Vendas).toFixed(2)}`)
+        for (let i = 0; i < categoriaAgora.length; i++) { //Percorre o categoriaAgora da categoria atual 
+            let produto = categoriaAgora[i] // O nome, vendas, e Preco do produto atual da categoria atual será armazenada nesta variavel
+            if (produto.Preco > 2000) { // se verdadeiro, envia na console a comissão extra total
+                console.log(`Produto: ${produto.nomeProduto} \nComissão extra total: R$${((produto.Preco * 0.03) * produto.Vendas).toFixed(2)}`)
             }
         }
     }
 }
+
+function produtoMaisCaroVendido(array) {
+    let maisCaro = array[0][0] //a variavel recebe um produto aleatorio
+    for (let a = 0; a < categorias.length; a++) { //percorre as categorias
+        let categoriaAgora = categorias[a] // armazena a categoria atual
+        for (let i = 0; i < categoriaAgora.length; i++) { // percorre os produtos da categoria
+            let produto = categoriaAgora[i] // armazena as informações do produto atual
+            if (produto.Preco > maisCaro.Preco) { //se o preço do produto atual for maior que o preço produto aleatoria 
+                maisCaro = produto //a variavel recebera as informações do produto atual
+            }
+        }
+    }
+    return `-----Produto mais caro vendido no mês----- \nProduto: ${maisCaro.nomeProduto} \nPreco: ${maisCaro.Preco}`
+}
+
 console.log(vendasMesTotal());
 
 console.log(vendasMesCategoria(categorias[0]));
@@ -53,3 +68,5 @@ console.log(vendasMesCategoria(categorias[1]));
 console.log(vendasMesCategoria(categorias[2]));
 
 listaComissão()
+
+console.log(produtoMaisCaroVendido(categorias));
